799efd56432a56c0cf7625ace4fc5112862cfb49
JaydipMagan <25053933+JaydipMagan@users.noreply.github.com>
2019-01-11 23:45:25 +0000

    added vim configuration and updated install script

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 echo "Installing config files"
-
-cp configfiles/.tmux.conf ~/
-
-echo "Finished"
+cp configfiles/.tmux.conf ~/.tmux.conf
+echo "Installed tmux configuration."
+cp configfiles/generate.vim ~/.vimrc
+echo "Installed vim configuration."
+mv ~/.bashrc ~/.bashrc-backup
+echo "Backed up .bashrc"
+cp configfiles/.bashrc ~/.bashrc
+echo "Installed bash configuration"
+vim +PlugInstall +qall
+echo "Installed vim plugins"
+echo "Finished workspace Installation."
 
