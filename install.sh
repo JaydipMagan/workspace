d376babc0844fa80fd3e9822edf9a8786379dbb7
JaydipMagan <25053933+JaydipMagan@users.noreply.github.com>
2020-08-09 00:25:23 +0100

    fixed install file

--- install.sh
+++ install.sh
@@ -4,13 +4,13 @@ echo "Installing tmux configuration files"
 cp configfiles/tmux/.tmux.conf ~/.tmux.conf
 cp configfiles/.tmux.conf.local ~/.tmux.conf.local
 echo "Installed tmux configuration."
-# cp configfiles/generate.vim ~/.vimrc
-# echo "Installed vim configuration."
-# vim +PlugInstall +qall
-# echo "Installed vim plugins"
-# mv ~/.bashrc ~/.bashrc-backup
-# echo "Backed up .bashrc"
-# cp configfiles/.bashrc ~/.bashrc
-# echo "Installed bashrc configuration"
-# echo "Finished workspace Installation."
+cp configfiles/generate.vim ~/.vimrc
+echo "Installed vim configuration."
+vim +PlugInstall +qall
+echo "Installed vim plugins"
+mv ~/.bashrc ~/.bashrc-backup
+echo "Backed up .bashrc"
+cp configfiles/.bashrc ~/.bashrc
+echo "Installed bashrc configuration"
+echo "Finished workspace Installation."
 
